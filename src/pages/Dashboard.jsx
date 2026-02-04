31ac55454b9263d6d3f60b90a7c1cbccf6f40966
anusin1805 <156580126+anusin1805@users.noreply.github.com>
2026-02-04 21:16:51 +0530

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { Sidebar } from "@/components/layout/Sidebar";
+import { Sidebar } from "@/components/layout/Slidebar";
 import { Header } from "@/components/layout/Header";
 import { StatsCard } from "@/components/ui/StatsCard";
 import { useFinancialStats } from "@/hooks/use-financial";
