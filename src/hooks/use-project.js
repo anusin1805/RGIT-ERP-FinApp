fff6af685d596793e6e53208f83a6b5909d2dcfe
anusin1805 <156580126+anusin1805@users.noreply.github.com>
2026-02-05 11:18:26 +0530

    Update use-project.js

--- src/hooks/use-project.js
+++ src/hooks/use-project.js
@@ -1,5 +1,5 @@
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
-import { api, type InsertMilestone, type InsertQcForm } from "@shared/routes";
+import { api } from "@shared/routes";
 
 export function useMilestones() {
   return useQuery({
