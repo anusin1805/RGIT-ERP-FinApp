d503b127b39e1158a645affb28aca89ae079e58b
anusin1805 <156580126+anusin1805@users.noreply.github.com>
2026-02-06 20:30:46 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -62,7 +62,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
-        <Toaster />
+        <Notifications />
         <Router />
       </TooltipProvider>
     </QueryClientProvider>
