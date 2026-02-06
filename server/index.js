2ffe437224b51efacd6d798a8b910d28a000d0e4
anusin1805 <156580126+anusin1805@users.noreply.github.com>
2026-02-06 11:46:33 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -53,8 +53,8 @@ app.use((req, res, next) => {
     serveStatic(app);
   }
 
-  const PORT = 5000;
-  server.listen(PORT, "0.0.0.0", () => {
-    log(`serving on port ${PORT}`);
-  });
+  const PORT = process.env.PORT || 5000;
+server.listen(PORT, "0.0.0.0", () => {
+  log(`serving on port ${PORT}`);
+});
 })();
