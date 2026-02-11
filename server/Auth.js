56d43cf95c4b4444b7f3257f6d1d1c3a98c90721
anusin1805 <156580126+anusin1805@users.noreply.github.com>
2026-02-11 15:15:37 +0530

    Update Auth.js

--- server/Auth.js
+++ server/Auth.js
@@ -120,7 +120,7 @@ export async function setupAuth(app) {
     req.logout(() => {
       res.redirect(
         client.buildEndSessionUrl(config, {
-          client_id: process.env!,
+          client_id: process.env,
           post_logout_redirect_uri: `${req.protocol}://${req.hostname}`,
         }).href
       );
