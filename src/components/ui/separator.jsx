878242f680998e6eb6f9d0d676cb72f0deef85b2
anusin1805 <156580126+anusin1805@users.noreply.github.com>
2026-02-12 19:24:57 +0530

    Update separator.jsx

--- src/components/ui/separator.jsx
+++ src/components/ui/separator.jsx
@@ -3,17 +3,11 @@ import * as SeparatorPrimitive from "@radix-ui/react-separator"
 
 import { cn } from "@/lib/utils"
 
-const Separator = React.forwardRef<
-  React.ElementRef<typeof SeparatorPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof SeparatorPrimitive.Root>
->(
-  (
-    { className, orientation = "horizontal", decorative = true, ...props },
-    ref
-  ) => (
+const Separator = React.forwardRef(
+  ({ className, orientation = "horizontal", ...props }, ref) => (
     <SeparatorPrimitive.Root
       ref={ref}
-      decorative={decorative}
+      decorative
       orientation={orientation}
       className={cn(
         "shrink-0 bg-border",
