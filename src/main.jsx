68f848fc8b00d1cdb6866fb42f9c77029a94a5b8
anusin1805 <156580126+anusin1805@users.noreply.github.com>
2026-02-12 13:16:24 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,11 @@
+import "./index.css";
+import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./index.css";
 
-createRoot(document.getElementById("root")).render(<App />);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
